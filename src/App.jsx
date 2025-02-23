a1e22f0ab3cae1a28c0580978a5a10e7501edcdc
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-23 17:21:23 +0530

    megablock

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     <h1>hello jii</h1>
+        
+    </>
+  )
+}
+
+export default App
