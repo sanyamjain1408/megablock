883f39ddca4e00312bc013270be1647602bf127e
Sanyam Jain <156403166+sanyamkanu@users.noreply.github.com>
2025-02-25 23:34:43 +0530

    megablock

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import {Header, Footer} from './components'
+import { Outlet } from 'react-router-dom'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>hello jii kese hai aap sab</h1>
-        
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'> 
+    <div className='w-full block'>
+      <Header />
+      <main>
+     Todo:  {/* <Outlet /> */}
+      </main>
+      <Footer />
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
